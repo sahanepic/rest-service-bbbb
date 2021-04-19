be72008ac6421d77641ce2a7eb755b8b1a8e1c40
sahanepic <sahanepic@gmail.com>
2021-04-19 17:00:51 +0530

    Third commit hnb-sms

--- src/main/java/com/sahanbcs/model/delivey/RequestDiliveryStatusReport.java
+++ src/main/java/com/sahanbcs/model/delivey/RequestDiliveryStatusReport.java
@@ -1,4 +1,14 @@
 package com.sahanbcs.model.delivey;
 
 public class RequestDiliveryStatusReport {
+
+    private String destinationAddress;
+    private String timeStamp;
+    private String requestId;
+    private String deliveryStatus;
+
+
+    public RequestDiliveryStatusReport() {
+        this.destinationAddress = destinationAddress;
+    }
 }
--- src/main/java/com/sahanbcs/model/recive/RequestReciveSMS.java
+++ src/main/java/com/sahanbcs/model/recive/RequestReciveSMS.java
@@ -1,4 +1,71 @@
 package com.sahanbcs.model.recive;
 
 public class RequestReciveSMS {
+
+    private String version;
+    private String applicationId;
+    private String sourceAddress;
+    private String message;
+    private String requestId;
+    private String encoding;
+
+    public RequestReciveSMS() {
+    }
+
+    public RequestReciveSMS(String version, String applicationId, String sourceAddress, String message, String requestId, String encoding) {
+        this.version = version;
+        this.applicationId = applicationId;
+        this.sourceAddress = sourceAddress;
+        this.message = message;
+        this.requestId = requestId;
+        this.encoding = encoding;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public String getSourceAddress() {
+        return sourceAddress;
+    }
+
+    public void setSourceAddress(String sourceAddress) {
+        this.sourceAddress = sourceAddress;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
 }
