edec6a2ccf707a45dc0e24a6eb29e5190b65751c
sahanepic <sahanepic@gmail.com>
2021-04-20 10:21:40 +0530

    Fifth  all class requeat and responce done in sms

---
+++ src/main/java/com/sahanbcs/model/Enomorators/RDiliveryStatus.java
@@ -0,0 +1,11 @@
+package com.sahanbcs.model.Enomorators;
+
+public enum RDiliveryStatus {
+    DELIVERED,
+    EXPIRED,
+    DELETED,
+    UNDELIVERABLE,
+    ACCEPTED,
+    UNKNOWN,
+    REJECTED
+}
